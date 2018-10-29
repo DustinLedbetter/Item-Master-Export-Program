e8c5970fb7155c3fa9497a552fbf05d64a912bd4
Dustin Ledbetter <dustinledbetter9@gmail.com>
2018-10-29 09:21:26 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Item_Master_Export_SQL
+{
+    partial class ItemMasterExport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ItemMasterExport));
+            this.MainButton = new System.Windows.Forms.Button();
+            this.WelcomeInfoBox = new System.Windows.Forms.TextBox();
+            this.SuccessFailLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ItemMasterRButton = new System.Windows.Forms.RadioButton();
+            this.InventoryExportRButton = new System.Windows.Forms.RadioButton();
+            this.StoredProcedureGBox = new System.Windows.Forms.GroupBox();
+            this.ServerChoiceGBox = new System.Windows.Forms.GroupBox();
+            this.ProdRButton = new System.Windows.Forms.RadioButton();
+            this.DevRButton = new System.Windows.Forms.RadioButton();
+            this.CommunicorpLogoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.StoredProcedureGBox.SuspendLayout();
+            this.ServerChoiceGBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CommunicorpLogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainButton
+            // 
+            this.MainButton.AccessibleDescription = "The main button that when pushed will run a query that then saves returned data t" +
+    "o the users desktop in a excel or CSV file";
+            this.MainButton.AccessibleName = "Main Button";
+            this.MainButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.MainButton.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.MainButton.Location = new System.Drawing.Point(118, 218);
+            this.MainButton.Name = "MainButton";
+            this.MainButton.Size = new System.Drawing.Size(85, 25);
+            this.MainButton.TabIndex = 0;
+            this.MainButton.Text = "Push To Start";
+            this.MainButton.UseVisualStyleBackColor = false;
+            this.MainButton.Click += new System.EventHandler(this.MainButton_Click);
+            // 
+            // WelcomeInfoBox
+            // 
+            this.WelcomeInfoBox.AccessibleDescription = "Text box that describes what will happen when the main button is pressed";
+            this.WelcomeInfoBox.AccessibleName = "Welcome Info Box";
+            this.WelcomeInfoBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WelcomeInfoBox.BackColor = System.Drawing.Color.White;
+            this.WelcomeInfoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeInfoBox.Location = new System.Drawing.Point(12, 12);
+            this.WelcomeInfoBox.Multiline = true;
+            this.WelcomeInfoBox.Name = "WelcomeInfoBox";
+            this.WelcomeInfoBox.ReadOnly = true;
+            this.WelcomeInfoBox.Size = new System.Drawing.Size(305, 75);
+            this.WelcomeInfoBox.TabIndex = 1;
+            this.WelcomeInfoBox.TabStop = false;
+            this.WelcomeInfoBox.Text = "Hi, Welcome to the Item Master Export Program:\r\n\r\n1.) Select Server \r\n2.) Select " +
+    "Stored Procedure\r\n3.) Click to export Item Master/Inventory from [Site]";
+            // 
+            // SuccessFailLabel
+            // 
+            this.SuccessFailLabel.AccessibleDescription = "This label will show up and display whether a succesful pull and save happened  o" +
+    "r not";
+            this.SuccessFailLabel.AccessibleName = "Success Fail Label";
+            this.SuccessFailLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.SuccessFailLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.SuccessFailLabel.Location = new System.Drawing.Point(105, 246);
+            this.SuccessFailLabel.Name = "SuccessFailLabel";
+            this.SuccessFailLabel.Size = new System.Drawing.Size(116, 13);
+            this.SuccessFailLabel.TabIndex = 2;
+            this.SuccessFailLabel.Text = "Button Not Pushed Yet";
+            this.SuccessFailLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SuccessFailLabel.Visible = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 267);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // ItemMasterRButton
+            // 
+            this.ItemMasterRButton.AccessibleDescription = "Item Master Radio Button";
+            this.ItemMasterRButton.AccessibleName = "Item Master Radio Button";
+            this.ItemMasterRButton.AccessibleRole = System.Windows.Forms.AccessibleRole.RadioButton;
+            this.ItemMasterRButton.AutoSize = true;
+            this.ItemMasterRButton.Checked = true;
+            this.ItemMasterRButton.Location = new System.Drawing.Point(5, 19);
+            this.ItemMasterRButton.Name = "ItemMasterRButton";
+            this.ItemMasterRButton.Size = new System.Drawing.Size(80, 17);
+            this.ItemMasterRButton.TabIndex = 7;
+            this.ItemMasterRButton.TabStop = true;
+            this.ItemMasterRButton.Text = "Item Master";
+            this.ItemMasterRButton.UseVisualStyleBackColor = true;
+            this.ItemMasterRButton.CheckedChanged += new System.EventHandler(this.ItemMasterRButton_CheckedChanged);
+            // 
+            // InventoryExportRButton
+            // 
+            this.InventoryExportRButton.AccessibleDescription = "Inventory Export Radio Button";
+            this.InventoryExportRButton.AccessibleName = "Inventory Export Radio Button";
+            this.InventoryExportRButton.AccessibleRole = System.Windows.Forms.AccessibleRole.RadioButton;
+            this.InventoryExportRButton.AutoSize = true;
+            this.InventoryExportRButton.Location = new System.Drawing.Point(5, 42);
+            this.InventoryExportRButton.Name = "InventoryExportRButton";
+            this.InventoryExportRButton.Size = new System.Drawing.Size(102, 17);
+            this.InventoryExportRButton.TabIndex = 8;
+            this.InventoryExportRButton.Text = "Inventory Export";
+            this.InventoryExportRButton.UseVisualStyleBackColor = true;
+            this.InventoryExportRButton.CheckedChanged += new System.EventHandler(this.InventoryExportRButton_CheckedChanged);
+            // 
+            // StoredProcedureGBox
+            // 
+            this.StoredProcedureGBox.Controls.Add(this.ItemMasterRButton);
+            this.StoredProcedureGBox.Controls.Add(this.InventoryExportRButton);
+            this.StoredProcedureGBox.Location = new System.Drawing.Point(207, 93);
+            this.StoredProcedureGBox.Name = "StoredProcedureGBox";
+            this.StoredProcedureGBox.Size = new System.Drawing.Size(110, 65);
+            this.StoredProcedureGBox.TabIndex = 12;
+            this.StoredProcedureGBox.TabStop = false;
+            this.StoredProcedureGBox.Text = "Stored Procedure:";
+            // 
+            // ServerChoiceGBox
+            // 
+            this.ServerChoiceGBox.AccessibleDescription = "Group box for server choice";
+            this.ServerChoiceGBox.AccessibleName = "Group box for server choice";
+            this.ServerChoiceGBox.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.ServerChoiceGBox.Controls.Add(this.ProdRButton);
+            this.ServerChoiceGBox.Controls.Add(this.DevRButton);
+            this.ServerChoiceGBox.Location = new System.Drawing.Point(12, 93);
+            this.ServerChoiceGBox.Name = "ServerChoiceGBox";
+            this.ServerChoiceGBox.Size = new System.Drawing.Size(100, 65);
+            this.ServerChoiceGBox.TabIndex = 11;
+            this.ServerChoiceGBox.TabStop = false;
+            this.ServerChoiceGBox.Text = "Server Choice:";
+            // 
+            // ProdRButton
+            // 
+            this.ProdRButton.AccessibleDescription = "Prod Radio Button";
+            this.ProdRButton.AccessibleName = "Prod Radio Button";
+            this.ProdRButton.AccessibleRole = System.Windows.Forms.AccessibleRole.RadioButton;
+            this.ProdRButton.AutoSize = true;
+            this.ProdRButton.Checked = true;
+            this.ProdRButton.Location = new System.Drawing.Point(6, 19);
+            this.ProdRButton.Name = "ProdRButton";
+            this.ProdRButton.Size = new System.Drawing.Size(74, 17);
+            this.ProdRButton.TabIndex = 9;
+            this.ProdRButton.TabStop = true;
+            this.ProdRButton.Text = "[Site] Prod";
+            this.ProdRButton.UseVisualStyleBackColor = true;
+            this.ProdRButton.CheckedChanged += new System.EventHandler(this.AvantiProdRButton_CheckedChanged);
+            // 
+            // DevRButton
+            // 
+            this.DevRButton.AccessibleDescription = "Dev Radio Button";
+            this.DevRButton.AccessibleName = "Dev Radio Button";
+            this.DevRButton.AccessibleRole = System.Windows.Forms.AccessibleRole.RadioButton;
+            this.DevRButton.AutoSize = true;
+            this.DevRButton.Location = new System.Drawing.Point(6, 42);
+            this.DevRButton.Name = "DevRButton";
+            this.DevRButton.Size = new System.Drawing.Size(72, 17);
+            this.DevRButton.TabIndex = 10;
+            this.DevRButton.Text = "[Site] Dev";
+            this.DevRButton.UseVisualStyleBackColor = true;
+            this.DevRButton.CheckedChanged += new System.EventHandler(this.AvantiDevRButton_CheckedChanged);
+            // 
+            // CommunicorpLogoPictureBox
+            // 
+            this.CommunicorpLogoPictureBox.AccessibleDescription = "Communicorp logo Picture Box";
+            this.CommunicorpLogoPictureBox.AccessibleName = "Communicorp logo Picture Box";
+            this.CommunicorpLogoPictureBox.BackColor = System.Drawing.Color.White;
+            this.CommunicorpLogoPictureBox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("CommunicorpLogoPictureBox.ErrorImage")));
+            this.CommunicorpLogoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("CommunicorpLogoPictureBox.Image")));
+            this.CommunicorpLogoPictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("CommunicorpLogoPictureBox.InitialImage")));
+            this.CommunicorpLogoPictureBox.Location = new System.Drawing.Point(118, 98);
+            this.CommunicorpLogoPictureBox.Name = "CommunicorpLogoPictureBox";
+            this.CommunicorpLogoPictureBox.Size = new System.Drawing.Size(83, 60);
+            this.CommunicorpLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CommunicorpLogoPictureBox.TabIndex = 13;
+            this.CommunicorpLogoPictureBox.TabStop = false;
+            // 
+            // ItemMasterExport
+            // 
+            this.AccessibleDescription = "A form that exports items from the DB and saves them in an excel file on the user" +
+    "\'s desktop when main button on the form is pushed.";
+            this.AccessibleName = "Item Master Export";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(329, 261);
+            this.Controls.Add(this.CommunicorpLogoPictureBox);
+            this.Controls.Add(this.ServerChoiceGBox);
+            this.Controls.Add(this.StoredProcedureGBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.SuccessFailLabel);
+            this.Controls.Add(this.WelcomeInfoBox);
+            this.Controls.Add(this.MainButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ItemMasterExport";
+            this.Text = "Item Master Export";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.StoredProcedureGBox.ResumeLayout(false);
+            this.StoredProcedureGBox.PerformLayout();
+            this.ServerChoiceGBox.ResumeLayout(false);
+            this.ServerChoiceGBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CommunicorpLogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MainButton;
+        private System.Windows.Forms.TextBox WelcomeInfoBox;
+        private System.Windows.Forms.Label SuccessFailLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton ItemMasterRButton;
+        private System.Windows.Forms.RadioButton InventoryExportRButton;
+        private System.Windows.Forms.GroupBox StoredProcedureGBox;
+        private System.Windows.Forms.GroupBox ServerChoiceGBox;
+        private System.Windows.Forms.RadioButton ProdRButton;
+        private System.Windows.Forms.RadioButton DevRButton;
+        private System.Windows.Forms.PictureBox CommunicorpLogoPictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,280 @@
+﻿/***********************************************************************************************************************************
+*                                                 GOD First                                                                        *
+* Author: Dustin Ledbetter                                                                                                         *
+* Release Date: 8-28-2018                                                                                                          *
+* Version: 4.3                                                                                                                     *
+* Purpose: An app where the user chooses between several servers and stored procedures and clicks the submit button.               *
+*    When the button is pushed the program runs there selection and retrieves data from a database, adds it to an excel file,      * 
+*    and asks the user where they would like to save it. The program can then be ran again with the same or new options selected.  *
+************************************************************************************************************************************/
+
+// Using Section
+using Excel = Microsoft.Office.Interop.Excel;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace Item_Master_Export_SQL
+{
+    public partial class ItemMasterExport : Form
+    {
+
+        public ItemMasterExport()
+        {
+            InitializeComponent();
+        }
+
+
+        // Declare and initialize variables used with Excel file processing
+        object misValue = System.Reflection.Missing.Value;
+        int i = 0;
+        int j = 0;
+        int cellRowIndex = 1;
+        int cellColumnIndex = 1;
+
+        // Declare and initialize variables used with SQL setup and processing
+        SqlConnection connection;
+        string storedProcedure = "";
+        string RBStoredProcedure1 = ConfigurationManager.AppSettings["ItemMaster"];
+        string RBStoredProcedure2 = ConfigurationManager.AppSettings["InventoryExport"];
+        // String RBStoredProcedure3 = ConfigurationManager.AppSettings["Testing"];
+        SqlDataAdapter dscmd = new SqlDataAdapter();
+        SqlConnection connectionProd = new SqlConnection(ConfigurationManager.ConnectionStrings["PROD"].ConnectionString);
+        SqlConnection connectionDev = new SqlConnection(ConfigurationManager.ConnectionStrings["DEV"].ConnectionString);
+
+
+        /// <summary>
+        ///  When the button on the app is clicked the code here:
+        ///   1.) connects to a specified database the user chooses,
+        ///   2.) runs a query to retrieve data from it, 
+        ///   3.) places data into an excel file,
+        ///   4.) saves it to where the user chooses
+        /// </summary>
+        private void MainButton_Click(object sender, EventArgs e)
+        {
+
+            // Used to reset label for when the user runs the same choices twice
+            SuccessFailLabel.Visible = false;
+
+            // Safety Net SQL
+            try
+            {
+
+                // Set default values for when the user clicks on button without changing the radio buttons first
+                if (connection == null && storedProcedure == "")
+                {
+                    connection = connectionProd;
+                    storedProcedure = RBStoredProcedure1;
+                }
+
+                // Set default values for when the user clicks on button but only changes the server choice radio button
+                else if (connection == connectionDev && storedProcedure == "")
+                {
+                    storedProcedure = RBStoredProcedure1;
+                }
+
+                // Set default values for when the user clicks on button but only changes the stored procedure radio button
+                else if (connection == null && storedProcedure == RBStoredProcedure2)
+                {
+                    connection = connectionProd;
+                }
+
+
+                // Using SQL declared variables above and radio button (Server Choice) and (Stored Procedure) choices the user chose:
+                // Run query and add data to a datatable and then to grid for keeping headers in excel
+                connection.Open();
+                dscmd = new SqlDataAdapter(storedProcedure, connection);
+                DataTable dt = new DataTable();
+                dscmd.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                // Safety Net Excel
+                try
+                {
+
+                    // Setup Excel workbook
+                    Excel.Application MyApp = new Excel.Application
+                    {
+                        Visible = false,
+                        DisplayAlerts = false
+                    };
+                    Excel._Workbook MyBook = (Excel._Workbook)(MyApp.Workbooks.Add(misValue));
+                    Excel._Worksheet MySheet = (Excel._Worksheet)MyBook.ActiveSheet;
+                    MySheet.Name = "Master Export File";
+                    int lastRow = MySheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+
+                    // Loop through each row and read value from each column. 
+                    // Add -1 to get row placed 1 below where headers are
+                    for (i = -1; i < dt.Rows.Count; i++)
+                    {
+                        for (j = 0; j < dt.Columns.Count; j++)
+                        {
+                            // Excel index starts from 1,1. As first Row would have the Column headers, adding a condition check. 
+                            if (cellRowIndex == 1)
+                            {
+                                MySheet.Cells[cellRowIndex, cellColumnIndex] = dataGridView1.Columns[j].HeaderText;
+                            }
+                            else
+                            {
+                                MySheet.Cells[cellRowIndex, cellColumnIndex] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                            }
+                            cellColumnIndex++;
+                        }
+                        // Keep column on 1 row, but allow rows to keep increasing
+                        cellColumnIndex = 1;
+                        cellRowIndex++;
+                    }
+
+                    try
+                    {
+                        // Open file save dialog and prompt user for save location
+                        SaveFileDialog saveDlg = new SaveFileDialog
+                        {
+                            InitialDirectory = @"C:\",
+                            FileName = storedProcedure + ".xls", // Set File name example to match the stored procedure used
+                            Filter = "Excel files (*.xls)|*.xls",
+                            FilterIndex = 0,
+                            RestoreDirectory = true,
+                            Title = "Export Excel File To"
+                        };
+
+                        // After user hits ok to location and file name; Save file and reset values for next use
+                        if (saveDlg.ShowDialog() == DialogResult.OK)
+                        {
+                            string path = saveDlg.FileName;
+                            MyBook.SaveAs(path, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlShared, misValue, misValue, misValue, misValue, misValue);
+                            MyBook.Saved = true;
+                            MyBook.Close(true, misValue, misValue);
+                            MyApp.Quit();
+                            Marshal.ReleaseComObject(MySheet);
+                            Marshal.ReleaseComObject(MyBook);
+
+                            // Update user of successful save
+                            SuccessFailLabel.Text = ("File Saved Properly");
+                            SuccessFailLabel.Visible = true;
+                        }
+                        else
+                        {
+                            // Update user of cancelled save
+                            SuccessFailLabel.Text = ("File Save Cancelled");
+                            SuccessFailLabel.Visible = true;
+                            MyBook.Close();
+                            MyApp.Quit();
+                            Marshal.ReleaseComObject(MySheet);
+                            Marshal.ReleaseComObject(MyBook);
+
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        // Alert user of failure to save excel file
+                        SuccessFailLabel.Text = ("File Not Saved Properly");
+                        SuccessFailLabel.Visible = true;
+                        MyBook.Close();
+                        MyApp.Quit();
+                        Marshal.ReleaseComObject(MySheet);
+                        Marshal.ReleaseComObject(MyBook);
+                        MessageBox.Show
+                            (
+                                ex.Message,
+                                "Excel Proccessing Issue: Failed to Save",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error
+                            );
+                    }
+                    finally
+                    {
+                        GC.Collect();
+
+                        // Close the connection and reset values for next use
+                        connection.Close(); 
+                        i = 0;
+                        j = 0;
+                        cellRowIndex = 1;
+                        cellColumnIndex = 1;
+                    }
+
+
+            
+                }
+                catch (Exception ex)
+                {
+                    // Alert user of failure to put into an excel file
+                    SuccessFailLabel.Text = ("File Not Saved");
+                    SuccessFailLabel.Visible = true;
+                    MessageBox.Show
+                        (
+                            ex.Message,
+                            "Excel Proccessing Issue: Failed to Export Data to Excel",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                        );
+                }
+                finally
+                {
+                    GC.Collect();
+                    connection.Close(); // Close the connection
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                // Alert user of Database connection failure
+                SuccessFailLabel.Text = ("File Not Saved Properly");
+                SuccessFailLabel.Visible = true;
+                MessageBox.Show
+                (
+                    ex.Message,
+                    "Database Issue: Failed to Save",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+            finally
+            {
+                GC.Collect();
+                connection.Close(); // Close the connection
+            }
+        }
+
+
+        // Sets the Prod Server button choice as active and resets successfail label
+        private void AvantiProdRButton_CheckedChanged(object sender, EventArgs e)
+        {
+            connection = connectionProd;
+            SuccessFailLabel.Visible = false;
+        }
+
+
+        // Sets the Dev Server button choice as active and resets successfail label
+        private void AvantiDevRButton_CheckedChanged(object sender, EventArgs e)
+        {
+            connection = connectionDev;
+            SuccessFailLabel.Visible = false;
+        }
+
+
+        // Sets the Master stored procedure button choice as active and resets successfail label
+        private void ItemMasterRButton_CheckedChanged(object sender, EventArgs e)
+        {
+            storedProcedure = RBStoredProcedure1;
+            SuccessFailLabel.Visible = false;
+        }
+
+
+        // Sets the Iventory Export stored procedure button choice as active and resets successfail label
+        private void InventoryExportRButton_CheckedChanged(object sender, EventArgs e)
+        {
+            storedProcedure = RBStoredProcedure2;
+            SuccessFailLabel.Visible = false;
+        }
+    }
+}
